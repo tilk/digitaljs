0253a1713e68907ecd2553bf809be11561b952e5
Marek Materzok <tilk@tilk.eu>
2018-07-30 17:27:02 +0200

    Copied gate definitions to the project.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 "use strict";
 
 import joint from 'jointjs';
+import './joint.js';
 import './style.css';
     
 function getCellType(tp) {
     const types = {
-        '$and': joint.shapes.logic.And,
-        '$or': joint.shapes.logic.Or,
-        '$xor': joint.shapes.logic.Xor,
-        '$not': joint.shapes.logic.Not
+        '$and': joint.shapes.digital.And,
+        '$or': joint.shapes.digital.Or,
+        '$xor': joint.shapes.digital.Xor,
+        '$not': joint.shapes.digital.Not
     };
     if (tp in types) return types[tp];
     else return null; // TODO
@@ -18,12 +19,21 @@ function makeGraph(data) {
     const graph = new joint.dia.Graph();
     for (const dev of data.devices) {
         const cellType = getCellType(dev.type);
-        const cell = new cellType({ position: {x: 0, y: 0} });
+        const cell = new cellType({ id: dev.id });
         graph.addCell(cell);
+    }
+    for (const conn of data.connectors) {
+        const src = conn.from.split('.');
+        const tgt = conn.to.split('.');
+        graph.addCell(new joint.shapes.digital.Wire({
+            source: {id: src[0], port: src[1]},
+            target: {id: tgt[0], port: tgt[1]},
+        }));
     }
     joint.layout.DirectedGraph.layout(graph, {
         nodeSep: 20,
         edgeSep: 30,
+        rankSep: 60,
         rankDir: "LR"
     });
     return graph;
@@ -40,9 +50,20 @@ export class Circuit {
             width: 1000, height: 600, gridSize: 5,
             snapLinks: true,
             linkPinning: false,
-            defaultLink: new joint.shapes.logic.Wire,
+            defaultLink: new joint.shapes.digital.Wire,
             validateConnection: function(vs, ms, vt, mt, e, vl) {
-                return true;
+                if (e === 'target') {
+                    if (!mt || !$(mt).hasClass('input'))
+                        return false;
+                    const link = this.model.getConnectedLinks(vt.model).find((l) =>
+                        l.id !== vl.model.id &&
+                        l.get('target').id === vt.model.id &&
+                        l.get('target').port === mt.getAttribute('port')
+                    );
+                    return !link;
+                } else {
+                    return true;
+                }
             }
         });
         this.graph.resetCells(this.graph.getCells());
